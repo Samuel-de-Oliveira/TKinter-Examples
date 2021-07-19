db51af38170223869840fe3e9a80db02e49be5ab
Samuel-de-Oliveira <81493939+Samuel-de-Oliveira@users.noreply.github.com>
2021-07-19 12:39:12 -0300

    Update Main.py

--- 1-User Registering Program/Main.py
+++ 1-User Registering Program/Main.py
@@ -5,7 +5,7 @@ from Window import mainWindow
 #   This is a window program who makes a .txt file    #
 #   with the data inputed in it, enjoy! :D            #
 #                                                     #
-#   Open the ABOUT.md file for more info.             #
+#   Open the README.md file for more info.            #
 #                                                     #
 #   Version: 1.1 (Future updates if this have bugs)   #
 #                                                     #
