6581bfd6fda9618f911e1c8b527938e8a451b18d
Samuel-de-Oliveira <samwolfg13@gmail.com>
2021-09-16 10:03:10 -0300

    Edit

--- Linux-Examples/1-Program-Executer/Main.py
+++ Linux-Examples/1-Program-Executer/Main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from colors import *
 
 class mainWindow():
     def __init__(self):
@@ -6,7 +7,12 @@ class mainWindow():
         Win = Tk()
         Win.resizable(False, False)
         Win.geometry('500x500')
-        Win['bg'] = 'blue'
+        Win['bg'] = color[0]
+        
+        # widgets:
+        Label(Win, text="Run an application here:", bg=color[0], fg=color[3]).place(x=0, y=0)
+        cmd = Entry(Win, bg=color[2]).pack()
+        Button(Win, bg=color[1], text='Run').place(x=10, y=100)
 
         Win.mainloop()
 
---
+++ Linux-Examples/1-Program-Executer/colors.py
@@ -0,0 +1,12 @@
+#-*--------------------------------------------------------------------------*-#
+#                                                                              #
+# This is a color library, is possible change if you want :3                   #
+# I have created a DarkMode theme but if you want you can make a LightMode by  #
+# yourself                                                                     #
+#                                                                              #
+#-*--------------------------------------------------------------------------*-#
+
+color = ['#4D4D4D', # Color 0
+        '#707070', # Color 1
+        '#9A9A9A', # Color 2
+        '#CDCDCD'] # Color 3
