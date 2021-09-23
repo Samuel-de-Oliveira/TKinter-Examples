3de96a2b00136d4ae6ca1a5ed2cdf11964a47227
Samuel-de-Oliveira <samwolfg13@gmail.com>
2021-09-23 11:17:28 -0300

    Linux examples

--- Linux-Examples/1-Program-Executer/runner.sh
+++ Linux-Examples/1-Program-Executer/runner.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+# If the command dont exist will show a #
+# meassage, and exit.                   #
 if [ ! -f /usr/bin/$1 ]; then
 	echo "This command does not exist!"
 	exit 1
 fi
-exec $*
+
+exec $* # Execute the command in entry.
