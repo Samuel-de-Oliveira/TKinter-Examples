7f93ebc3733724a15b6c9062e712f3e76675066d
Samuel de Oliveira <81493939+Samuel-de-Oliveira@users.noreply.github.com>
2022-05-28 00:20:53 -0300

    Remove clear

--- Linux-Examples/1-Program-Executer/runner.sh
+++ Linux-Examples/1-Program-Executer/runner.sh
@@ -2,9 +2,8 @@
 
 # If the command don't exist will show a #
 # meassage, and exit.                    #
-clear
 if [ ! -f /usr/bin/$1 ]; then
-	echo "This command does not exist!"
+	echo "This command doesn't exist!"
 	exit 1
 fi
 
