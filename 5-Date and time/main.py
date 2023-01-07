5f7ff8f80f839275fb608f9a4eb5ecb38056c83f
Samuel-de-Oliveira <samwolfg13@gmail.com>
2023-01-06 21:54:49 -0300

    Add the datetime framework in project

--- 5-Date and time/main.py
+++ 5-Date and time/main.py
@@ -1,16 +1,20 @@
 from tkinter import *
 from colors import  *
+from datetime import datetime, timedelta
 
-class window():
+Today = datetime.now().date()
+
+class mainWindow():
     def __init__(self) -> None:
         self.Win = Tk() # Main window object
         self.Win['bg'] = color[0] # Background
         self.Win.geometry('500x300') # Geometry
         self.Win.resizable(False, False) # Can't resize
 
+        Label(text=Today, fg=color[3], bg=color[0]).pack()
         Button(text='Refresh', fg=color[3], bg=color[1], command=self.hello).place(x=222, y=260)
         self.Win.mainloop() # Main loop
 
     def hello(self): print("Hello, world!")
 
-window()
+if __name__ == '__main__': mainWindow()
